5dafd343876de8576dd13146e2718e906cf7036b
unknown <adrian.gh.m.18@gmail.com>
2018-12-27 16:11:12 +0200

    Commit Message

--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -8,7 +8,7 @@ const initialState = {
     selectedPlace : null
 };
 
-// if we don't have a state we start with initial state
+//if we don't have a state we start with initial state
 const reducer = (state = initialState, action) => {
     switch( action.type ) {
         case ADD_PLACE :
